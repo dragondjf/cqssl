86d4d69654c79650646d7672d580abf9dccf6c98
dragondjf <ding465398889@163.com>
2016-12-08 21:20:23 +0800

    udpate

--- analyze.py
+++ analyze.py
@@ -59,20 +59,35 @@ class DayModel(object):
 
     def computer(self):
         for key in self.keys[0:-1]:
-            for x in xrange(4, 30):
+            for x in xrange(1, 30):
                 datas = AnalyzeTool.small(getattr(self, key), x)
                 if len(datas) > 0:
                     # print key , x, len(datas)
                     getattr(self, key + "result")[x] = len(datas)
 
-    def printResult(self):
+    def printResult(self, start=4, end=11):
         count = 0
+        m = 0
         for key in self.keys[0:-1]:
             # print getattr(self, key)
             ret = getattr(self, key + "result")
-            print key, ret
+            # print key, ret
             count += sum(ret.values())
-        return count
+
+            lost = 0
+            for x in range(0, end - start + 1):
+                lost += 10* pow(2, x)
+
+            # print lost
+            for key in ret.keys():
+                if key < start:
+                    continue
+                if key <= end:
+                    m += ret[key]*10 * 0.95
+                else:
+                    m -= lost * ret[key]
+
+        return count, m
 
 
 def date_range(start_date, end_date):
@@ -84,15 +99,20 @@ def date_range(start_date, end_date):
 def main():
     # dbWorker.loadData("app/cqssc.txt")
 
-    begin = datetime.datetime(2016, 8, 1)
+    begin = datetime.datetime(2016, 1, 1)
     end = datetime.datetime(2016, 8, 31)
 
-    for i in date_range(begin, end):
-        day = int(i.strftime('%Y%m%d'))
-        print "============" + str(day) + "================"
-        dayModel = DayModel(day)
-        ret = dayModel.printResult()
-        print "============" + str(ret) + "================"
+    for t in range(17, 30):
+        money = []
+        for i in date_range(begin, end):
+            day = int(i.strftime('%Y%m%d'))
+            # print "============" + str(day) + "================"
+            dayModel = DayModel(day)
+            ret, m = dayModel.printResult(17, t)
+            money.append(m)
+            # print "============" + str(ret) + "  " + str(m) + "================"
+        print "***************" + str(t) + "   " + str(sum(money)) + "************************"
+
 
 
 if __name__ == '__main__':
